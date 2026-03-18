f3bbf5b3fc1a8884dc0015d99b6c301f9976310e
rafaalmeida1 <rafael.almeida@backlgrs.com.br>
2026-03-17 23:33:46 -0300

    Atualiza URL do backend para o endpoint de produção

--- worker-docs/src/index.ts
+++ worker-docs/src/index.ts
@@ -27,7 +27,7 @@ export default {
 			}
 
 			// chama backend
-			const response = await fetch("http://localhost:3002/api/validate", {
+			const response = await fetch("https://workers-edge-api.vercel.app/api/validate", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json"
